d9df3081d1ae5571e3214297447209d1f627c583
Sandeep Kushwaha <sandeepkush327@gmail.com>
2024-10-22 14:44:41 +0530

    Remove Extra White Spaces from a String

--- src/string/RemoveWhiteSpacesFromString.java
+++ src/string/RemoveWhiteSpacesFromString.java
@@ -2,6 +2,10 @@ package string;
 
 public class RemoveWhiteSpacesFromString {
 
+    /**
+     * Main method of the class for the following question:
+     *   Write a Java program to remove white spaces from a string.
+     * */
     public static void main(String[] args) {
         String str = "  Hello    World!,     I love       Java    Programming     language.    ";
         System.out.println("Original String: " + str);
